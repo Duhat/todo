23875c9f2200eb35ad67c4226ac3cc582b1ef50c
Duhat <oksanashevchuk990@gmail.com>
2024-07-26 20:33:38 +0300

    added save function

--- script.js
+++ script.js
@@ -11,6 +11,7 @@ function add(){
         let span = document.createElement("span");
         span.innerHTML = "\u00d7";
         li.appendChild(span);
+        save();
         
     }
     inputBox.value = '';
@@ -18,9 +19,22 @@ function add(){
 }
 
 list.addEventListener("click", function(e) {
-    if(e.target.tagName === "LI")
+    if(e.target.tagName === "LI"){
         e.target.classList.toggle('checked');
+        save();
+    }
     else if(e.target.tagName === "SPAN"){
         e.target.parentElement.remove();
+        save();
     }
+    
 } );
+
+function save(){
+    localStorage.setItem('data', list.innerHTML);
+}
+
+function load(){
+    list.innerHTML = localStorage.getItem('data')
+}
+load();
